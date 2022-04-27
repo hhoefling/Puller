328f0c21455434b3d217f3d74b428f48657ceb53
hhoefling <penny@gmx.eu>
2022-04-27 13:40:21 +0200

    V0.01

--- puller.php
+++ puller.php
@@ -1,7 +1,6 @@
 <?php
 
 
-header('Content-Type: text/plain');
    
    
 function logf($s)
@@ -10,41 +9,20 @@ function logf($s)
 } 
  
  
- $retwith= ( isset($_GET['retwith']) ? $_GET['retwith'] : ''); 
- $pullfrom= ( isset($_GET['pullfrom']) ? $_GET['pullfrom'] : ''); 
-
- $pull= ( isset($_GET['pull']) ? $_GET['pull'] : ''); 
- $pullx = explode(",", $pull);
- $mods=[];
- $mods['lp1']=0;
- $mods['lp2']=0;
- $mods['ll1']=1;	// da hängt der puller per html dran
- $mods['ll2']=0;
- $mods['soc1']=0;
- $mods['soc2']=0;
- $mods['evu']=0;
- $mods['wr1']=0;
- $mods['wr2']=0;
- $mods['bat']=0;
- foreach( $pullx as $p)  $mods[$p]=true;
- logf( print_r($mods,true) );
-
- logf("retwith:$retwith pullfrom:$pullfrom pull:$pull");
- 		  
-		    
+function getopenwbconfig($fn)
+{
+  global $debug;
   
- $myConfigFile = $_SERVER['DOCUMENT_ROOT'].'/openWB/openwb.conf';
-
+  
 // prepare key/value array
- $settingsArray = [];
-
-  try {
-		if ( !file_exists($myConfigFile) ) {
-			throw new Exception('Konfigurationsdatei nicht gefunden.');
-		 }
+    $settingsArray = [];
+    try {
+		  if ( !file_exists($fn) ) {
+		  	throw new Exception('Konfigurationsdatei nicht gefunden.');
+		  }
 		 
 		// first read config-lines in array
- 		$settingsFile = file($myConfigFile);
+ 		$settingsFile = file($fn);
 
 	 	foreach($settingsFile as $line) 
  		{
@@ -54,59 +32,123 @@ function logf($s)
 			$splitLine[0] = trim($splitLine[0]);
 			$splitLine[1] = trim($splitLine[1]); // do not trim single quotes, we will need them later
 			// push key/value pair to new array
-			if( preg_match("/mqtt/" , $splitLine[1]) )
+			if(   preg_match("/mqtt/" , $splitLine[1]) 
+               || $splitLine[0]=='debug'
+               || $splitLine[0]=='lastmanagement'
+               || $splitLine[0]=='ladeleistungmodul'
+               )
 				$settingsArray[$splitLine[0]] = $splitLine[1];
 		}
-    } catch ( Exception $e ) {
+        $debug=$settingsArray['debug'];
+
+    } 
+    catch ( Exception $e ) 
+    {
 		$msg = $e->getMessage();
-		echo "<script>alert('$msg');</script>";
+		logf("$msg");
 	}	
- logf( print_r($settingsArray, true) );
-/*
-    [evsecon] => mqttevse
-    [evsecons1] => mqttevse
-    [ladeleistungs1modul] => mqttlllp2
-    [wattbezugmodul] => bezug_mqtt
-    [pvwattmodul] => wr_mqtt
-    [socmodul] => soc_mqtt
-    [socmodul1] => soc_mqtt
-    [speichermodul] => speicher_mqtt
+    if ($debug>1) 
+       logf( print_r($settingsArray, true) );
+    return $settingsArray;
+}
 
-*/ 
- if( $mods['lp1']  &&   $settingsArray['evsecon'] != 'mqttevse' ) $mods['lp1']=0;
- if( $mods['lp2']  &&   $settingsArray['evsecons1'] != 'mqttevse' ) $mods['lp2']=0;
- // imemer da hier der puller dranghängt if( $mods['ll1']  &&   $settingsArray['ladeleistungmodul'] != 'mqttevse' ) $mods['ll1']=0;
- if( $mods['ll2']  &&   $settingsArray['ladeleistungs1modul'] != 'mqttlllp2' ) $mods['ll2']=0;
- if( $mods['evu']  &&   $settingsArray['wattbezugmodul'] != 'bezug_mqtt' ) $mods['evu']=0;
- if( $mods['wr1']  &&   $settingsArray['pvwattmodul'] != 'wr_mqtt' ) $mods['wr1']=0;
- if( $mods['wr2']  &&   $settingsArray['pvwattmodul2'] != 'wr_mqtt2' ) $mods['wr2']=0;
- if( $mods['bat']  &&   $settingsArray['speichermodul'] != 'speicher_mqtt' ) $mods['bat']=0;
-  
- logf( print_r($mods,true) );
- $sum=0;
- foreach($mods as $m)
-    if($m==1) 
-      $sum++;
- if($sum>0)
-  {
-   logf(" $sum aktiver module, starte daemon");
-   exec("./puller.sh 1 &", $output, $retval);
-  }
-  else
-  {
-   logf(" keine aktiven module, Stoppe daemon");
-   exec("./puller/puller.sh 0 &", $output, $retval);
-  }
- logf(print_r($output,true));
-  
-   
-# exec("./soc_citigo/getsoc.sh $para >>./soc_citigo/getsoc.log 2>&1 & ", $output,$retval);
-# echo "Returned with status $retval and output:\n";
-# print_r($output);
-# echo "\n";
+
+
+ $retwith=  ( isset($_GET['retwith']) ? escapeshellcmd($_GET['retwith']) : 'lla3'); 
+ $pullfrom= ( isset($_GET['pullfrom']) ? escapeshellarg($_GET['pullfrom']) : ''); 
+ $pull= ( isset($_GET['pull']) ? escapeshellcmd($_GET['pull']) : ''); 
+ $myConfigFile = $_SERVER['DOCUMENT_ROOT'].'/openWB/openwb.conf';
+ $settingsArray = getopenwbconfig($myConfigFile);
+ $debug=$settingsArray['debug'];
  
+ if($retwith>'')
+ {
+    header('Content-Type: text/plain');
+ 
+    $pullx = explode(",", $pull);
+    $mods=[];
+    $mods['lp1']=0;
+    $mods['lp2']=0;
+    $mods['ll1']=1;	// da hängt der puller per html dran
+    $mods['ll2']=0;
+    $mods['soc1']=0;
+    $mods['soc2']=0;
+    $mods['evu']=0;
+    $mods['wr1']=0;
+    $mods['wr2']=0;
+    $mods['bat']=0;
+    foreach( $pullx as $p) 
+        $mods[$p]=1;
+        
+    if ($debug>2) 
+        logf( print_r($mods,true) );
 
- # Nehme letzten ermittelten Wert mit zurueck, warte nicht auf das Ende der aktuellen abfrage
- $x  = file_get_contents("/var/www/html/openWB/ramdisk/$retwith");
- echo "$x"
+    logf("retwith:$retwith pullfrom:$pullfrom pull:$pull");
+     if( $settingsArray['lastmanagement'] > 0 )
+     {
+      if( $mods['lp2']  &&   $settingsArray['evsecons1'] != 'mqttevse' ) $mods['lp2']=0;  
+      if( $mods['ll2']  &&   $settingsArray['ladeleistungs1modul'] != 'mqttlllp2' ) $mods['ll2']=0;
+      if( $mods['soc2'] &&   $settingsArray['socmodul1'] != 'soc_mqtt' ) $mods['soc2']=0;
+     } else
+     {
+      $mods['lp2']=0;  
+      $mods['ll2']=0;
+      $mods['soc2']=0;
+     }
+     if( $mods['soc1'] &&   $settingsArray['socmodul'] != 'soc_mqtt' ) $mods['soc1']=0;
+     if( $mods['lp1']  &&   $settingsArray['evsecon'] != 'mqttevse' ) $mods['lp1']=0;
+     // imemer drin da hier der puller dranghängt if( $mods['ll1']  &&   $settingsArray['ladeleistungmodul'] != 'httpll' ) $mods['ll1']=0;
+     if( $mods['evu']  &&   $settingsArray['wattbezugmodul'] != 'bezug_mqtt' ) $mods['evu']=0;
+     if( $mods['wr1']  &&   $settingsArray['pvwattmodul'] != 'wr_mqtt' ) $mods['wr1']=0;
+     if( $mods['wr2']  &&   $settingsArray['pvwattmodul2'] != 'wr_mqtt2' ) $mods['wr2']=0;
+     if( $mods['bat']  &&   $settingsArray['speichermodul'] != 'speicher_mqtt' ) $mods['bat']=0;
+  
+    if ($debug>2) 
+       logf( print_r($mods,true) );
+    $sum=0;
+    foreach($mods as $m)
+        if($m==1) 
+            $sum++;
+    #$sum=0;
+    if($sum>0)
+    {
+        logf(" $sum aktiver module, starte Pull Daemon");
+        unset($output);
+        exec("ps -aux | grep -v grep | grep [p]uller.py | awk '{print $2}' ", $output, $retval);
+        if($debug>2) logf(print_r($output,true));
+        if( count($output)==0)
+        {
+           logf(" no activ puller, start one");
+            unset($output);
+            exec("sudo -u pi python3 ./puller.py $pullfrom >/var/www/html/openWB/ramdisk/puller.log 2>&1 &", $output, $retval);
+            if ($debug>1) 
+                logf(print_r($output,true));
+        } else
+        {
+           logf("puller allready running.");
+        }
+    }
+    else
+    {
+        logf(" keine aktiven module, Stoppe Pulll Daemon");
+        unset($output);
+        exec("ps -aux | grep -v grep | grep [p]uller.py | awk '{print $2}' ", $output, $retval);
+        if($debug>2) logf(print_r($output,true));
+        foreach($output as $pid)
+        {
+          $pid=intval($pid);
+          if($pid>0 )
+            {
+               unset($output);
+               exec("sudo kill $pid",$output, $retval);
+               logf('kill '.print_r($output, true));
+            }
+        }
+    }
+
+    $x  = file_get_contents("/var/www/html/openWB/ramdisk/$retwith");
+    echo "$x";
+}
+
+
 ?>
